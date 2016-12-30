7e8aee20854a8600a632f66afdea4936240c1bc8
Jonathan Jenkyn <jonathan@belial.co.uk>
2016-12-30 13:11:43 +0000

    Initial Release

---
+++ lambda_function.py
@@ -0,0 +1,427 @@
+from __future__ import print_function
+import re
+import random
+
+signoff = ["goodbye","exit","quit","leave"]
+toods = ["Thank you for talking with me.",
+      "Good-bye.",
+      "Thank you, that will be $150.  Have a good day!"]
+## Debug Helper
+
+def dumpclean(obj):
+    if type(obj) == dict:
+        for k, v in obj.items():
+            if hasattr(v, '__iter__'):
+                print (k)
+                dumpclean(v)
+            else:
+                print ('%s : %s' % (k, v))
+    elif type(obj) == list:
+        for v in obj:
+            if hasattr(v, '__iter__'):
+                dumpclean(v)
+            else:
+                print (v)
+    else:
+        print (obj)
+
+# --------------- Helpers that build all of the responses ----------------------
+
+def build_speechlet_response(title, output, reprompt_text, should_end_session):
+    if len(output)==0:
+        raise ValueError("Invalid output value of empty")
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': output
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': reprompt_text
+            }
+        },
+        'shouldEndSession': should_end_session
+    }
+    
+def build_welcome_response():
+    return {
+        'outputSpeech': {
+            'type': 'PlainText',
+            'text': 'This is Eliza, the therapy chatbot. This tool does not provide medical advice, and is for informational and educational purposes only, and is not a substitute for professional medical advice, treatment or diagnosis. Call your doctor to receive medical advice. If you think you may have a medical emergency, call your local emergency services. Hello. How are you feeling today?'
+        },
+        'card': {
+            'type': 'Simple',
+            'title':  'ELIZA for Alexa',
+            'content': 'This is Eliza, the therapy chatbot. This tool does not provide medical advice, and is for informational and educational purposes only, and is not a substitute for professional medical advice, treatment or diagnosis. Call your doctor to receive medical advice. If you think you may have a medical emergency, call your local emergency services. See https://en.wikipedia.org/wiki/ELIZA for more information.'
+        },
+        'reprompt': {
+            'outputSpeech': {
+                'type': 'PlainText',
+                'text': 'Please speak up. How are you feeling today?'
+            }
+        },
+        'shouldEndSession': False
+    }
+
+
+def build_response(session_attributes, speechlet_response):
+    return {
+        'version': '1.0',
+        'sessionAttributes': session_attributes,
+        'response': speechlet_response
+    }
+    
+# --------------- Functions that control the skill's behavior ------------------
+
+def get_welcome_response():
+
+    session_attributes = {}
+    card_title = "Eliza for Alexa"
+    speech_output = ""
+    # If the user either does not reply to the welcome message or says something
+    # that is not understood, they will be prompted again with this text.
+    reprompt_text = "Sorry. You'll have to speak up. How are you feeling today?"
+    should_end_session = False
+    return build_response(session_attributes, build_welcome_response())
+
+
+def handle_session_end_request():
+    card_title = "Eliza for Alexa session over"
+    speech_output = "That's the end of our session. Goodbye."
+    # Setting this to true ends the session and exits the skill.
+    should_end_session = True
+    return build_response({}, build_speechlet_response(
+        card_title, speech_output, None, should_end_session))
+        
+# --------------- Eliza Core ---------------------------------------------------
+
+reflections = {
+    "am": "are",
+    "was": "were",
+    "i": "you",
+    "i'd": "you would",
+    "i've": "you have",
+    "i'll": "you will",
+    "my": "your",
+    "are": "am",
+    "you've": "I have",
+    "you'll": "I will",
+    "your": "my",
+    "yours": "mine",
+    "you": "me",
+    "me": "you"
+}
+
+psychobabble = [
+    [r'I need (.*)',
+     ["Why do you need {0}?",
+      "Would it really help you to get {0}?",
+      "Are you sure you need {0}?"]],
+
+    [r'Why don\'?t you ([^\?]*)\??',
+     ["Do you really think I don't {0}?",
+      "Perhaps eventually I will {0}.",
+      "Do you really want me to {0}?"]],
+
+    [r'Why can\'?t I ([^\?]*)\??',
+     ["Do you think you should be able to {0}?",
+      "If you could {0}, what would you do?",
+      "I don't know -- why can't you {0}?",
+      "Have you really tried?"]],
+
+    [r'I can\'?t (.*)',
+     ["How do you know you can't {0}?",
+      "Perhaps you could {0} if you tried.",
+      "What would it take for you to {0}?"]],
+
+    [r'I am (.*)',
+     ["Did you come to me because you are {0}?",
+      "How long have you been {0}?",
+      "How do you feel about being {0}?"]],
+
+    [r'I\'?m (.*)',
+     ["How does being {0} make you feel?",
+      "Do you enjoy being {0}?",
+      "Why do you tell me you're {0}?",
+      "Why do you think you're {0}?"]],
+
+    [r'Are you ([^\?]*)\??',
+     ["Why does it matter whether I am {0}?",
+      "Would you prefer it if I were not {0}?",
+      "Perhaps you believe I am {0}.",
+      "I may be {0} -- what do you think?"]],
+
+    [r'What (.*)',
+     ["Why do you ask?",
+      "How would an answer to that help you?",
+      "What do you think?"]],
+
+    [r'How (.*)',
+     ["How do you suppose?",
+      "Perhaps you can answer your own question.",
+      "What is it you're really asking?"]],
+
+    [r'Because (.*)',
+     ["Is that the real reason?",
+      "What other reasons come to mind?",
+      "Does that reason apply to anything else?",
+      "If {0}, what else must be true?"]],
+
+    [r'(.*) sorry (.*)',
+     ["There are many times when no apology is needed.",
+      "What feelings do you have when you apologize?"]],
+
+    [r'Hello(.*)',
+     ["Hello... I'm glad you could drop by today.",
+      "Hi there... how are you today?",
+      "Hello, how are you feeling today?"]],
+
+    [r'I think (.*)',
+     ["Do you doubt {0}?",
+      "Do you really think so?",
+      "But you're not sure {0}?"]],
+
+    [r'(.*) friend (.*)',
+     ["Tell me more about your friends.",
+      "When you think of a friend, what comes to mind?",
+      "Why don't you tell me about a childhood friend?"]],
+
+    [r'Yes',
+     ["You seem quite sure.",
+      "OK, but can you elaborate a bit?"]],
+
+    [r'(.*) computer(.*)',
+     ["Are you really talking about me?",
+      "Does it seem strange to talk to a computer?",
+      "How do computers make you feel?",
+      "Do you feel threatened by computers?"]],
+
+    [r'Is it (.*)',
+     ["Do you think it is {0}?",
+      "Perhaps it's {0} -- what do you think?",
+      "If it were {0}, what would you do?",
+      "It could well be that {0}."]],
+
+    [r'It is (.*)',
+     ["You seem very certain.",
+      "If I told you that it probably isn't {0}, what would you feel?"]],
+
+    [r'Can you ([^\?]*)\??',
+     ["What makes you think I can't {0}?",
+      "If I could {0}, then what?",
+      "Why do you ask if I can {0}?"]],
+
+    [r'Can I ([^\?]*)\??',
+     ["Perhaps you don't want to {0}.",
+      "Do you want to be able to {0}?",
+      "If you could {0}, would you?"]],
+
+    [r'You are (.*)',
+     ["Why do you think I am {0}?",
+      "Does it please you to think that I'm {0}?",
+      "Perhaps you would like me to be {0}.",
+      "Perhaps you're really talking about yourself?"]],
+
+    [r'You\'?re (.*)',
+     ["Why do you say I am {0}?",
+      "Why do you think I am {0}?",
+      "Are we talking about you, or me?"]],
+
+    [r'I don\'?t (.*)',
+     ["Don't you really {0}?",
+      "Why don't you {0}?",
+      "Do you want to {0}?"]],
+
+    [r'I feel (.*)',
+     ["Good, tell me more about these feelings.",
+      "Do you often feel {0}?",
+      "When do you usually feel {0}?",
+      "When you feel {0}, what do you do?"]],
+
+    [r'I have (.*)',
+     ["Why do you tell me that you've {0}?",
+      "Have you really {0}?",
+      "Now that you have {0}, what will you do next?"]],
+
+    [r'I would (.*)',
+     ["Could you explain why you would {0}?",
+      "Why would you {0}?",
+      "Who else knows that you would {0}?"]],
+
+    [r'Is there (.*)',
+     ["Do you think there is {0}?",
+      "It's likely that there is {0}.",
+      "Would you like there to be {0}?"]],
+
+    [r'My (.*)',
+     ["I see, your {0}.",
+      "Why do you say that your {0}?",
+      "When your {0}, how do you feel?"]],
+
+    [r'You (.*)',
+     ["We should be discussing you, not me.",
+      "Why do you say that about me?",
+      "Why do you care whether I {0}?"]],
+
+    [r'Why (.*)',
+     ["Why don't you tell me the reason why {0}?",
+      "Why do you think {0}?"]],
+
+    [r'I want (.*)',
+     ["What would it mean to you if you got {0}?",
+      "Why do you want {0}?",
+      "What would you do if you got {0}?",
+      "If you got {0}, then what would you do?"]],
+
+    [r'(.*) mother(.*)',
+     ["Tell me more about your mother.",
+      "What was your relationship with your mother like?",
+      "How do you feel about your mother?",
+      "How does this relate to your feelings today?",
+      "Good family relations are important."]],
+
+    [r'(.*) father(.*)',
+     ["Tell me more about your father.",
+      "How did your father make you feel?",
+      "How do you feel about your father?",
+      "Does your relationship with your father relate to your feelings today?",
+      "Do you have trouble showing affection with your family?"]],
+
+    [r'(.*) child(.*)',
+     ["Did you have close friends as a child?",
+      "What is your favorite childhood memory?",
+      "Do you remember any dreams or nightmares from childhood?",
+      "Did the other children sometimes tease you?",
+      "How do you think your childhood experiences relate to your feelings today?"]],
+
+    [r'(.*)\?',
+     ["Why do you ask that?",
+      "Please consider whether you can answer your own question.",
+      "Perhaps the answer lies within yourself?",
+      "Why don't you tell me?"]],
+
+    [r'quit',
+     ["Thank you for talking with me.",
+      "Good-bye.",
+      "Thank you, that will be $150.  Have a good day!"]],
+
+    [r'(.*)',
+     ["Please tell me more.",
+      "Let's change focus a bit... Tell me about your family.",
+      "Can you elaborate on that?",
+      "Why do you say that {0}?",
+      "I see.",
+      "Very interesting.",
+      "{0}.",
+      "I see.  And what does that tell you?",
+      "How does that make you feel?",
+      "How do you feel when you say that?"]]
+]
+
+
+def reflect(fragment):
+    tokens = fragment.lower().split()
+    for i, token in enumerate(tokens):
+        if token in reflections:
+            tokens[i] = reflections[token]
+    return ' '.join(tokens)
+
+
+def analyze(statement):
+    for pattern, responses in psychobabble:
+        match = re.match(pattern.lower(), statement.lower().rstrip(".!"))
+        if match:
+            response = random.choice(responses)
+            return response.format(*[reflect(g) for g in match.groups()])
+
+
+def eliza_response(intent,session):
+    ## main intent manager
+    session_attributes = {}
+    reprompt_text = None
+    dumpclean(intent)
+    if session['new']:
+        return get_welcome_response()
+    elif intent['slots']['patient'].has_key("value") and intent['slots']['patient']['value'] in signoff:
+        should_end_session = True
+        speech_output = random.choice(toods)
+        return build_response(session_attributes, build_speechlet_response(
+            intent['name'], speech_output, reprompt_text, should_end_session))
+    else:
+        if(intent['slots']['patient'].has_key("value")==False):
+            raise ValueError("intent['slots']['patient'].has_key('value')==False Booo Hiss")
+        speech_output = analyze(intent['slots']['patient']['value'])
+        reprompt_text = "Sorry you'll have to speak up. " + speech_output
+        should_end_session = False
+        
+        return build_response(session_attributes, build_speechlet_response(
+            intent['name'], speech_output, reprompt_text, should_end_session))
+        
+# ----------------Events ------------------------
+
+def on_session_started(session_started_request, session):
+    """ Called when the session starts """
+    print("on_session_started requestId=" + session_started_request['requestId']
+          + ", sessionId=" + session['sessionId'])
+
+def on_launch(launch_request, session):
+
+    print("on_launch requestId=" + launch_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # Dispatch to your skill's launch
+    return get_welcome_response()
+    
+def on_intent(intent_request, session):
+
+    print("on_intent requestId=" + intent_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+
+    intent = intent_request['intent']
+    intent_name = intent_request['intent']['name']
+
+    # Dispatch to your skill's intent handlers
+    if intent_name == "Eliza":
+        return eliza_response(intent, session)
+    elif intent_name == "AMAZON.HelpIntent":
+        return get_welcome_response()
+    elif intent_name == "AMAZON.CancelIntent" or intent_name == "AMAZON.StopIntent":
+        return handle_session_end_request()
+    else:
+        raise ValueError("Invalid intent")
+
+def on_session_ended(session_ended_request, session):
+    print("on_session_ended requestId=" + session_ended_request['requestId'] +
+          ", sessionId=" + session['sessionId'])
+    # add cleanup logic here
+
+# --------------- Main handler ------------------
+
+def lambda_handler(event, context):
+    """ Route the incoming request based on type (LaunchRequest, IntentRequest,
+    etc.) The JSON body of the request is provided in the event parameter.
+    """
+    print("event.session.application.applicationId=" +
+          event['session']['application']['applicationId'])
+
+    """
+    Uncomment this if statement and populate with your skill's application ID to
+    prevent someone else from configuring a skill that sends requests to this
+    function.
+    """
+    if (event['session']['application']['applicationId']!="amzn1.ask.skill.93c5811d-1785-47c1-b499-bee2367ad071"):
+        raise ValueError("Invalid Application ID")
+    
+    if event['session']['new']:
+        on_session_started({'requestId': event['request']['requestId']},
+                           event['session'])
+
+    ##print ("Caught " + event['request']['type'])
+    if event['request']['type'] == "LaunchRequest":
+        return on_launch(event['request'], event['session'])
+    elif event['request']['type'] == "IntentRequest":
+        return on_intent(event['request'], event['session'])
+    elif event['request']['type'] == "SessionEndedRequest":
+        return on_session_ended(event['request'], event['session'])
+    else:
+        raise ValueError("Did not expect a "+event['request']['type']+" event")
